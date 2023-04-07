781d28a9b54742e65fd6f6e2c06e07c3d6bdcf90
Santi Liwsakphaiboon <oofpokemon@gmail.com>
2023-04-07 13:06:00 +0700

    chore: update gitignore build

--- functions/lib/funcs/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.listUsers = exports.createUser = void 0;
-const createUser_1 = require("./createUser");
-Object.defineProperty(exports, "createUser", { enumerable: true, get: function () { return createUser_1.createUser; } });
-const listUsers_1 = require("./listUsers");
-Object.defineProperty(exports, "listUsers", { enumerable: true, get: function () { return listUsers_1.listUsers; } });
-//# sourceMappingURL=index.js.map
