9854131d2700f93966abdb7dd527a558ac113681
Santi Liwsakphaiboon <oofpokemon@gmail.com>
2023-06-03 21:38:28 +0700

    test: config test

--- functions/src/__tests__/apis/setAdminRole.test.ts
+++ functions/src/__tests__/apis/setAdminRole.test.ts
@@ -1,10 +1,15 @@
-import { setAdminRole } from "../../apis";
-import AdminService from "../../core/services/adminService";
-jest.mock("../../core/services/adminService")
+import { setAdminRole } from '../../apis';
+import AdminService from '../../core/services/adminService';
+jest.mock('../../core/services/adminService');
 
 describe('Given setAdminRole is called', () => {
+  beforeEach(() => {
+
+  })
+
   describe('When call with invalid request body', () => {
     it('Then return error 400', () => {
+
       expect(400).toBe(400);
     })
   })
